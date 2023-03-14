afcadcdd86adcdad6b06d4f09832a4b1c8f52ec0
JereDev19 <jeremiaserba7894@gmail.com>
2023-03-14 15:07:46 -0300

    Task 2 - Project 2 Initializing witch 0

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *space;
+	int *space, iterator;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -22,6 +22,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (space == NULL)
 		return (NULL);
 
+	for (iterator = 0; iterator < nmemb; iterator++)
+	{
+		space[iterator] = 0;
+	}
 	return (space);
 	free(space);
 }
