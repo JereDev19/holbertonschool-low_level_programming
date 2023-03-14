9bfe6b4f44b87cc2b7894515a415263419c9a323
JereDev19 <jeremiaserba7894@gmail.com>
2023-03-14 16:46:26 -0300

    Task 3 - Project 2 Chages in the declaration malloc

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,5 +1,15 @@
 #include "main.h"
 
+/**
+ * array_range - Create an array of integers of min to max.
+ *
+ * @min: First min range.
+ * @max: Second max range.
+ * Return: pointer pointen to the specific memory.
+ *
+ * Author: Jeremias Ebra (tremendo vivido si).
+ */
+
 int *array_range(int min, int max)
 {
 	int *ptro, iterator;
@@ -11,8 +21,8 @@ int *array_range(int min, int max)
 	if (ptro == NULL)
 		return (NULL);
 
-	for (iterator = min; iterator <= max; iterator++)
-		ptro[iterator] = iterator;
+	for (iterator = 0; min <= max; iterator++, min++)
+		ptro[iterator] = min;
 
 	return (ptro);
 }
